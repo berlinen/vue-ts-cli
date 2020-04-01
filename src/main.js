a2efbbceb636aaab383c90a1bdee386fc81e9a8e
“berlinen” <1292393108@qq.com>
2020-04-01 10:10:26 +0800

    feat: 项目创建

--- src/main.js
+++ src/main.js
@@ -6,3 +6,12 @@ import create from './create.js' // 项目创建
 import init from './init.js' // 项目初始化
 import dev from './dev' // 项目启动
 import build from './build' // 项目打包
+
+/**
+ * template-cli 命令列表
+ * actionMap => obj
+ */
+
+ let actionMap = {
+  
+ }
