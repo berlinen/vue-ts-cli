84880cea913bada6f9706ab66339eeeb812211ee
“berlinen” <1292393108@qq.com>
2020-04-01 09:56:15 +0800

    feat: 依赖引入

--- src/main.js
+++ src/main.js
@@ -2,3 +2,7 @@ import program from 'commander'
 import symbol from 'log-symbols'
 import chalk from 'chalk'
 
+import create from './create.js' // 项目创建
+import init from './init.js' // 项目初始化
+import dev from './dev' // 项目启动
+import build from './build' // 项目打包
