5eaee366c4699c0364a9252dff2162cd9ba1b737
“berlinen” <1292393108@qq.com>
2020-04-02 09:49:37 +0800

    feat: delete

--- src/main.js
+++ src/main.js
@@ -106,3 +106,16 @@ import build from './build.js' // 项目打包
           }
         })
  })
+
+ // 项目版本
+ program
+      .version(require('../package.json').version, '-v --version')
+      .parse(process.argv)
+
+
+/**
+ * template-cli ,命令后不带参数的时候，输出帮助信息
+ */
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
